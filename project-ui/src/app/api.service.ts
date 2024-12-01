ecbb14abe67c26f972ca906a3ce881de8c1c0c24
melodyyu <48936471+melodyyu@users.noreply.github.com>
2024-11-30 18:28:46 -0800

    set base url and see if that's the issue

--- project-ui/src/app/api.service.ts
+++ project-ui/src/app/api.service.ts
@@ -1,20 +1,53 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ApiService {
+
+//   constructor(private http: HttpClient) { }
+
+//   calculateProbability(userInput: any) {
+//     const response = this.http.post('/api/probability', userInput)
+//     return response
+//   }
+
+//   calculateClassification(userInput: any) {
+//     const response = this.http.post('/api/classification', userInput)
+//     return response
+//   }
+// }
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private baseUrl = 'https://cs6440-project-cloned.onrender.com'; // Ensure this is your backend URL
 
   constructor(private http: HttpClient) { }
 
-  calculateProbability(userInput: any) {
-    const response = this.http.post('/api/probability', userInput)
-    return response
+  /**
+   * Calls the API to calculate probability
+   * @param userInput - The data to send to the backend
+   * @returns Observable with the API response
+   */
+  calculateProbability(userInput: any): Observable<any> {
+    const endpoint = `${this.baseUrl}/api/probability`;
+    return this.http.post(endpoint, { userInput });
   }
 
-  calculateClassification(userInput: any) {
-    const response = this.http.post('/api/classification', userInput)
-    return response
+  /**
+   * Calls the API to calculate classification
+   * @param userInput - The data to send to the backend
+   * @returns Observable with the API response
+   */
+  calculateClassification(userInput: any): Observable<any> {
+    const endpoint = `${this.baseUrl}/api/classification`;
+    return this.http.post(endpoint, { userInput });
   }
 }
