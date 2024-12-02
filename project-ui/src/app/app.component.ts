4fedc99fac54dfc4a1769e57958c45c54d37f445
Melody Yu <melody.yu12@gmail.com>
2024-12-01 23:44:28 -0800

    front end is fully up

--- project-ui/src/app/app.component.ts
+++ project-ui/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { UserInputComponent } from './user-input/user-input.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [UserInputComponent],
+  imports: [UserInputComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
