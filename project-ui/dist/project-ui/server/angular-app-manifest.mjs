ed29418e7d0bb29d1225a0aea742eb02617cb193
Melody Yu <melody.yu12@gmail.com>
2024-12-02 01:27:18 -0800

    adding dist

--- project-ui/dist/project-ui/server/angular-app-manifest.mjs
+++ project-ui/dist/project-ui/server/angular-app-manifest.mjs
@@ -2,16 +2,10 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  routes: [
-  {
-    "renderMode": 2,
-    "route": "/"
-  }
-],
+  routes: undefined,
   assets: new Map([
-['index.csr.html', {size: 493, hash: '8421f3aecf2708df20b440792832e15ee730198025ceb4ad6a0bb961a845daf3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1006, hash: '1f55164027f236fe1f52fe69a47a21001e4ed3e7c05379436f1381c1847fad04', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
-['index.html', {size: 4012, hash: 'b56127e1ec264ba14725afcf85cac1f10d2f0acc3e44e2fd6058389147e106f7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 647, hash: '2a2ca045c10d55bcf1b4cc3369b18a26052c84250b05ea17d9781fd86b0caa2d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1160, hash: '97d510cade905a27be2f32a072657afb62adf2cc6412bdd52fb580e42d62dfba', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
 ['styles-5INURTSO.css', {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
--- project-ui/dist/project-ui/server/assets-chunks/index_csr_html.mjs
+++ project-ui/dist/project-ui/server/assets-chunks/index_csr_html.mjs
@@ -5,10 +5,10 @@ export default `<!doctype html>
   <title>ProjectUi</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-VMNRF5MK.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-NGHB6PI2.js" type="module"></script></body>
 </html>
 `;
